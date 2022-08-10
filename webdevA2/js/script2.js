5b13a1291249487e0df502355efd5a24f5386c39
blank-feed <shengheng75@gmail.com>
2022-08-11 02:11:37 +0800

    uedau

--- webdevA2/js/script2.js
+++ webdevA2/js/script2.js
@@ -12,3 +12,15 @@ function myFunction() {
   }
 
   
+  var z=window.matchMedia("(min-width: 540px)");
+  z.addEventListener("change",match)
+
+  function match() {
+    var x = document.getElementById("myTopnav");
+    if (z.matches == true) { // If media query matches
+      x.className = "directory menu";
+      console.log("egrs");
+      myFunction();
+    } 
+  }
+  
