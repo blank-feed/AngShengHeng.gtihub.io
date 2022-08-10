b21c6494ac7440425e5ac09b798403c884bf5d5a
blank-feed <shengheng75@gmail.com>
2022-08-11 02:06:05 +0800

    wyeuhv

--- webdevA2/js/script2.js
+++ webdevA2/js/script2.js
@@ -3,10 +3,12 @@ var menu=document.querySelector(".menu");
 menu.addEventListener("click",function(){myFunction()});
 myFunction();
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav");
     if (x.className === "directory menu") {
       x.className = "directory responsive";
     } else {
       x.className = "directory menu";
     }
   }
+
+  
